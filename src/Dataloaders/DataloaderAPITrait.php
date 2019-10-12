e0e63f7e4e4382714627bbfb3956384055b3aad0
Leo <losoviz@gmail.com>
2019-10-12 17:44:50 +0800

    Moved class to API

---
+++ src/Dataloaders/DataloaderAPITrait.php
@@ -0,0 +1,22 @@
+<?php
+namespace PoP\API\Dataloaders;
+use PoP\ComponentModel\Facades\Managers\ModuleProcessorManagerFacade;
+
+trait DataloaderAPITrait
+{
+    public function maybeFilterDataloadQueryArgs(array &$query, array $options = [])
+    {
+        // // Accept field atts to filter the API fields
+        // $vars = \PoP\ComponentModel\Engine_Vars::getVars();
+        // if (!\PoP\ComponentModel\Server\Utils::disableAPI() && $vars['scheme'] == POP_SCHEME_API) {
+        if ($filterDataloadQueryArgsParams = $options['filter-dataload-query-args']) {
+            $filterDataloadQueryArgsSource = $filterDataloadQueryArgsParams['source'];
+            $filterDataloadingModule = $filterDataloadQueryArgsParams['module'];
+            if ($filterDataloadQueryArgsSource && $filterDataloadingModule) {
+                $moduleprocessor_manager = ModuleProcessorManagerFacade::getInstance();
+                $moduleprocessor_manager->getProcessor($filterDataloadingModule)->filterHeadmoduleDataloadQueryArgs($filterDataloadingModule, $query, $filterDataloadQueryArgsSource);
+            }
+        }
+        // }
+    }
+}
