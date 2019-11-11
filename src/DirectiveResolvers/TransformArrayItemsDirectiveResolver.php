e5701f7b78f23ee0d2a7536c3cd3e1a04d7eea79
Leo <losoviz@gmail.com>
2019-11-11 17:39:45 +0800

    Renamed variables to expressions

--- src/DirectiveResolvers/TransformArrayItemsDirectiveResolver.php
+++ src/DirectiveResolvers/TransformArrayItemsDirectiveResolver.php
@@ -78,7 +78,7 @@ class TransformArrayItemsDirectiveResolver extends TransformPropertyDirectiveRes
         $arrayItemIdsProperties = [];
 
         // 1. Unpack all elements of the array into a property for each
-        // By making the property "propertyName:key", the "key" can be extracted and passed under variable `$key` to the function
+        // By making the property "propertyName:key", the "key" can be extracted and passed under expression `%key%` to the function
         foreach ($idsDataFields as $id => $dataFields) {
             foreach ($dataFields['direct'] as $field) {
                 $fieldOutputKey = $fieldQueryInterpreter->getFieldOutputKey($field);
