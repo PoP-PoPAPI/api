a3b35f2b67b08a1a6a6cb9c5ea210c9cd75ace4a
Leo <losoviz@gmail.com>
2019-11-22 20:02:18 +0800

    Removed unneeded imports

--- src/DirectiveResolvers/NoCacheCacheControlDirectiveResolver.php
+++ src/DirectiveResolvers/NoCacheCacheControlDirectiveResolver.php
@@ -1,8 +1,6 @@
 <?php
 namespace PoP\API\DirectiveResolvers;
 
-use PoP\ComponentModel\DataloaderInterface;
-use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 use PoP\CacheControl\DirectiveResolvers\AbstractCacheControlDirectiveResolver;
 
 class NoCacheCacheControlDirectiveResolver extends AbstractCacheControlDirectiveResolver
