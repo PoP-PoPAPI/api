ae9f9fe7e94634114613b3ddd7387d390916a291
Leo <losoviz@gmail.com>
2019-11-22 19:20:55 +0800

    Do not cache field getJSON

---
+++ src/DirectiveResolvers/NoCacheCacheControlDirectiveResolver.php
@@ -0,0 +1,22 @@
+<?php
+namespace PoP\API\DirectiveResolvers;
+
+use PoP\ComponentModel\DataloaderInterface;
+use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
+use PoP\CacheControl\DirectiveResolvers\AbstractCacheControlDirectiveResolver;
+
+class NoCacheCacheControlDirectiveResolver extends AbstractCacheControlDirectiveResolver
+{
+    public static function getFieldNamesToApplyTo(): array
+    {
+        return [
+            'getJSON',
+        ];
+    }
+
+    public function getMaxAge(DataloaderInterface $dataloader, FieldResolverInterface $fieldResolver, array &$resultIDItems, array &$idsDataFields, array &$dbItems, array &$previousDBItems, array &$variables, array &$messages, array &$dbErrors, array &$dbWarnings, array &$schemaErrors, array &$schemaWarnings, array &$schemaDeprecations): int
+    {
+        // Do not cache
+        return 0;
+    }
+}
