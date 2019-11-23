01cd1ded690cfa3a78dbba6eedb0a7fceee65e0e
Leo <losoviz@gmail.com>
2019-11-23 16:08:56 +0800

    Moved field "extract" to Engine

--- src/FieldValueResolvers/OperatorOrHelperFieldValueResolver.php
+++ src/FieldValueResolvers/OperatorOrHelperFieldValueResolver.php
@@ -1,24 +1,18 @@
 <?php
 namespace PoP\API\FieldValueResolvers;
 
-use PoP\ComponentModel\ErrorUtils;
 use PoP\GuzzleHelpers\GuzzleHelpers;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
 use PoP\ComponentModel\FieldValueResolvers\AbstractOperatorOrHelperFieldValueResolver;
-use PoP\API\Misc\OperatorHelpers;
-use Exception;
 
 class OperatorOrHelperFieldValueResolver extends AbstractOperatorOrHelperFieldValueResolver
 {
-    public const ERRORCODE_PATHNOTREACHABLE = 'path-not-reachable';
-
     public static function getFieldNamesToResolve(): array
     {
         return [
             'getJSON',
-            'extract',
         ];
     }
 
@@ -26,7 +20,6 @@ class OperatorOrHelperFieldValueResolver extends AbstractOperatorOrHelperFieldVa
     {
         $types = [
             'getJSON' => SchemaDefinition::TYPE_OBJECT,
-            'extract' => SchemaDefinition::TYPE_MIXED,
         ];
         return $types[$fieldName] ?? parent::getSchemaFieldType($fieldResolver, $fieldName);
     }
@@ -36,7 +29,6 @@ class OperatorOrHelperFieldValueResolver extends AbstractOperatorOrHelperFieldVa
         $translationAPI = TranslationAPIFacade::getInstance();
         $descriptions = [
             'getJSON' => $translationAPI->__('Retrieve data from URL and decode it as a JSON object', 'pop-component-model'),
-            'extract' => $translationAPI->__('Given an object, it retrieves the data under a certain path', 'pop-component-model'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($fieldResolver, $fieldName);
     }
@@ -54,43 +46,9 @@ class OperatorOrHelperFieldValueResolver extends AbstractOperatorOrHelperFieldVa
                         SchemaDefinition::ARGNAME_MANDATORY => true,
                     ],
                 ];
-            case 'extract':
-                return [
-                    [
-                        SchemaDefinition::ARGNAME_NAME => 'object',
-                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_OBJECT,
-                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The object to retrieve the data from', 'pop-component-model'),
-                        SchemaDefinition::ARGNAME_MANDATORY => true,
-                    ],
-                    [
-                        SchemaDefinition::ARGNAME_NAME => 'path',
-                        SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_STRING,
-                        SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The path to retrieve data from the object. Paths are separated with \'.\' for each sublevel', 'pop-component-model'),
-                        SchemaDefinition::ARGNAME_MANDATORY => true,
-                    ],
-                ];
         }
 
         return parent::getSchemaFieldArgs($fieldResolver, $fieldName);
-    }
-
-    protected function getDataFromPathError(string $fieldName, string $errorMessage)
-    {
-        return ErrorUtils::getError(
-            $fieldName,
-            self::ERRORCODE_PATHNOTREACHABLE,
-            $errorMessage
-        );
-    }
-
-    protected function getDataFromPath(string $fieldName, array $data, string $path)
-    {
-        try {
-            $dataPointer = OperatorHelpers::getPointerToArrayItemUnderPath($data, $path);
-        } catch (Exception $e) {
-            return $this->getDataFromPathError($fieldName, $e->getMessage());
-        }
-        return $dataPointer;
     }
 
     public function resolveValue(FieldResolverInterface $fieldResolver, $resultItem, string $fieldName, array $fieldArgs = [])
@@ -98,8 +56,6 @@ class OperatorOrHelperFieldValueResolver extends AbstractOperatorOrHelperFieldVa
         switch ($fieldName) {
             case 'getJSON':
                 return GuzzleHelpers::requestJSON($fieldArgs['url'], [], 'GET');
-            case 'extract':
-                return $this->getDataFromPath($fieldName, $fieldArgs['object'], $fieldArgs['path']);
         }
         return parent::resolveValue($fieldResolver, $resultItem, $fieldName, $fieldArgs);
     }
