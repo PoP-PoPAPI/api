742a48a7d4289128d4c4d5da710b46cc14db897f
Leo <losoviz@gmail.com>
2019-11-26 17:42:51 +0800

    Dynamically change max-age for "__schema"

--- src/Component.php
+++ src/Component.php
@@ -44,6 +44,6 @@ class Component extends AbstractComponent
         // Initialize classes
         ContainerBuilderUtils::instantiateNamespaceServices(__NAMESPACE__.'\\Hooks');
         ContainerBuilderUtils::attachFieldValueResolversFromNamespace(__NAMESPACE__.'\\FieldValueResolvers');
-        ContainerBuilderUtils::attachDirectiveResolversFromNamespace(__NAMESPACE__.'\\DirectiveResolvers');
+        ContainerBuilderUtils::attachDirectiveResolversFromNamespace(__NAMESPACE__.'\\DirectiveResolvers', false);
     }
 }
