da9a29d8ef85b9b02f616e8ebc0d84e1bea0f8e4
Leo <losoviz@gmail.com>
2019-12-27 18:55:19 +0800

    Migrated ModuleProcessor for Root

---
+++ src/ModuleProcessors/RootRelationalFieldDataloadModuleProcessor.php
@@ -0,0 +1,39 @@
+<?php
+namespace PoP\API\ModuleProcessors;
+
+use PoP\API\ModuleProcessors\AbstractRelationalFieldDataloadModuleProcessor;
+use PoP\API\TypeResolvers\RootTypeResolver;
+
+class RootRelationalFieldDataloadModuleProcessor extends AbstractRelationalFieldDataloadModuleProcessor
+{
+    public const MODULE_DATALOAD_RELATIONALFIELDS_ROOT = 'dataload-relationalfields-root';
+
+    public function getModulesToProcess(): array
+    {
+        return array(
+            [self::class, self::MODULE_DATALOAD_RELATIONALFIELDS_ROOT],
+        );
+    }
+
+    public function getDBObjectIDOrIDs(array $module, array &$props, &$data_properties)
+    {
+        switch ($module[1]) {
+            case self::MODULE_DATALOAD_RELATIONALFIELDS_ROOT:
+                return 'root';
+        }
+        return parent::getDBObjectIDOrIDs($module, $props, $data_properties);
+    }
+
+    public function getTypeResolverClass(array $module): ?string
+    {
+        switch ($module[1]) {
+            case self::MODULE_DATALOAD_RELATIONALFIELDS_ROOT:
+                return RootTypeResolver::class;
+        }
+
+        return parent::getTypeResolverClass($module);
+    }
+}
+
+
+
--- src/RouteModuleProcessors/EntryRouteModuleProcessor.php
+++ src/RouteModuleProcessors/EntryRouteModuleProcessor.php
@@ -3,6 +3,7 @@ namespace PoP\API\RouteModuleProcessors;
 
 use PoP\Routing\RouteNatures;
 use PoP\ModuleRouting\AbstractEntryRouteModuleProcessor;
+use PoP\API\ModuleProcessors\RootRelationalFieldDataloadModuleProcessor;
 
 class EntryRouteModuleProcessor extends AbstractEntryRouteModuleProcessor
 {
@@ -11,7 +12,7 @@ class EntryRouteModuleProcessor extends AbstractEntryRouteModuleProcessor
         $ret = array();
 
         $ret[RouteNatures::HOME][] = [
-            'module' => [\PoP_API_Module_Processor_FieldDataloads::class, \PoP_API_Module_Processor_FieldDataloads::MODULE_DATALOAD_DATAQUERY_ROOT_FIELDS],
+            'module' => [RootRelationalFieldDataloadModuleProcessor::class, RootRelationalFieldDataloadModuleProcessor::MODULE_DATALOAD_RELATIONALFIELDS_ROOT],
             'conditions' => [
                 'scheme' => POP_SCHEME_API,
             ],
