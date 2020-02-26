c6fc98983216d6dfbeee1474d6d266a9120b5289
Leo <losoviz@gmail.com>
2020-02-26 21:56:40 +0800

    Moved "private schema" env variable to AccessControl

--- src/Environment.php
+++ src/Environment.php
@@ -7,9 +7,5 @@ class Environment
     {
         return isset($_ENV['DISABLE_API']) ? strtolower($_ENV['DISABLE_API']) == "true" : false;
     }
-    public static function usePrivateSchemaMode(): bool
-    {
-        return isset($_ENV['USE_PRIVATE_SCHEMA_MODE']) ? strtolower($_ENV['USE_PRIVATE_SCHEMA_MODE']) == "true" : false;
-    }
 }
 
