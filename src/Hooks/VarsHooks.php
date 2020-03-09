c51f25a43e5a6da10ba72e9a3c04b7ef644262a0
Leo <losoviz@gmail.com>
2020-03-10 00:25:58 +0800

    Changed priority to allow others to execute before

--- src/Hooks/VarsHooks.php
+++ src/Hooks/VarsHooks.php
@@ -15,11 +15,11 @@ class VarsHooks extends AbstractHookSet
 {
     protected function init()
     {
-        // Execute immediately, since others (eg: SPA) will be based on these updated values
+        // Execute early, since others (eg: SPA) will be based on these updated values
         $this->hooksAPI->addAction(
             '\PoP\ComponentModel\Engine_Vars:addVars',
             array($this, 'addVars'),
-            0,
+            5,
             1
         );
         // Add functions as hooks, so we allow PoP_Application to set the 'routing-state' first
@@ -42,32 +42,32 @@ class VarsHooks extends AbstractHookSet
     public function addVars($vars_in_array)
     {
         $vars = &$vars_in_array[0];
-        if ($vars['scheme'] == POP_SCHEME_API) {
+        if ($vars['scheme'] == \POP_SCHEME_API) {
             // For the API, the response is always JSON
-            $vars['output'] = GD_URLPARAM_OUTPUT_JSON;
+            $vars['output'] = \GD_URLPARAM_OUTPUT_JSON;
 
             // Fetch datasetmodulesettings: needed to obtain the dbKeyPath to know where to find the database entries
             $vars['dataoutputitems'] = [
-                GD_URLPARAM_DATAOUTPUTITEMS_DATASETMODULESETTINGS,
-                GD_URLPARAM_DATAOUTPUTITEMS_MODULEDATA,
-                GD_URLPARAM_DATAOUTPUTITEMS_DATABASES,
+                \GD_URLPARAM_DATAOUTPUTITEMS_DATASETMODULESETTINGS,
+                \GD_URLPARAM_DATAOUTPUTITEMS_MODULEDATA,
+                \GD_URLPARAM_DATAOUTPUTITEMS_DATABASES,
             ];
 
             // dataoutputmode => Combined: there is no need to split the sources, then already combined them
-            $vars['dataoutputmode'] = GD_URLPARAM_DATAOUTPUTMODE_COMBINED;
+            $vars['dataoutputmode'] = \GD_URLPARAM_DATAOUTPUTMODE_COMBINED;
 
             // dboutputmode => Combined: needed since we don't know under what database does the dbKeyPath point to. Then simply integrate all of them
             // Also, needed for REST/GraphQL APIs since all their data comes bundled all together
-            $vars['dboutputmode'] = GD_URLPARAM_DATABASESOUTPUTMODE_COMBINED;
+            $vars['dboutputmode'] = \GD_URLPARAM_DATABASESOUTPUTMODE_COMBINED;
 
             // Only the data stratum is needed
             $platformmanager = StratumManagerFactory::getInstance();
-            $vars['stratum'] = POP_STRATUM_DATA;
+            $vars['stratum'] = \POP_STRATUM_DATA;
             $vars['strata'] = $platformmanager->getStrata($vars['stratum']);
             $vars['stratum-isdefault'] = $vars['stratum'] == $platformmanager->getDefaultStratum();
 
             // Do not print the entry module
-            $vars['actions'][] = POP_ACTION_REMOVE_ENTRYMODULE_FROM_OUTPUT;
+            $vars['actions'][] = \POP_ACTION_REMOVE_ENTRYMODULE_FROM_OUTPUT;
         }
     }
 
@@ -76,7 +76,7 @@ class VarsHooks extends AbstractHookSet
         // Allow WP API to set the "routing-state" first
         // Each page is an independent configuration
         $vars = &$vars_in_array[0];
-        if ($vars['scheme'] == POP_SCHEME_API) {
+        if ($vars['scheme'] == \POP_SCHEME_API) {
             $this->addFieldsToVars($vars);
         }
 
@@ -104,7 +104,7 @@ class VarsHooks extends AbstractHookSet
         // Allow WP API to set the "routing-state" first
         // Each page is an independent configuration
         $vars = Engine_Vars::getVars();
-        if ($vars['scheme'] == POP_SCHEME_API) {
+        if ($vars['scheme'] == \POP_SCHEME_API) {
             $this->addFieldsToComponents($components);
         }
 
