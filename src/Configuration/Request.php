ce1b1da122a375381cc4d1d1f340e9d3db7bc8e6
Leo <losoviz@gmail.com>
2020-04-05 10:09:29 +0800

    Typed function

--- src/Configuration/Request.php
+++ src/Configuration/Request.php
@@ -4,7 +4,7 @@ namespace PoP\API\Configuration;
 class Request
 {
     const URLPARAM_USE_NAMESPACE = 'use_namespace';
-    public static function namespaceTypesAndInterfaces()
+    public static function namespaceTypesAndInterfaces(): bool
     {
         return isset($_REQUEST[self::URLPARAM_USE_NAMESPACE]) && $_REQUEST[self::URLPARAM_USE_NAMESPACE];
     }
