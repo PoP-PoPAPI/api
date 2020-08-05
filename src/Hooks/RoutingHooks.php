a3d8eb17a70fdd19444ac4b658172d562b060269
Leonardo Losoviz <leo@getpop.org>
2020-08-05 17:35:51 +0800

    Throw exception if server misconfigured

--- src/Hooks/RoutingHooks.php
+++ src/Hooks/RoutingHooks.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PoP\API\Hooks;
 
+use Exception;
 use PoP\Engine\Hooks\AbstractHookSet;
 use PoP\ComponentModel\Misc\RequestUtils;
 
@@ -17,10 +18,20 @@ class RoutingHooks extends AbstractHookSet
         );
     }
 
-    public function getURIRoute($route)
+    public function getURIRoute(string $route): string
     {
         $cmsengineapi = \PoP\Engine\FunctionAPIFactory::getInstance();
-        $homeurl = $cmsengineapi->getHomeURL();
-        return substr(RequestUtils::getCurrentUrl(), strlen($homeurl));
+        $homeURL = $cmsengineapi->getHomeURL();
+        $currentURL = RequestUtils::getCurrentUrl();
+        // If the homeURL is not contained in the current URL
+        // then there's a misconfiguration in the server
+        if (substr($currentURL, 0, strlen($homeURL)) != $homeURL) {
+            throw new Exception(sprintf(
+                'The webserver is not configured properly, since the current URL \'%s\' does not contain the home URL \'%s\' (possibly the server name has not been set-up correctly)',
+                $currentURL,
+                $homeURL
+            ));
+        }
+        return substr($currentURL, strlen($homeURL));
     }
 }
