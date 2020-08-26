050a47b556799c75bff63a75b646f3220a03f5c7
Leonardo Losoviz <leo@getpop.org>
2020-08-26 16:32:16 +0800

    Enable to add extra properties to cache the schema

--- src/Cache/CacheUtils.php
+++ src/Cache/CacheUtils.php
@@ -4,18 +4,25 @@ declare(strict_types=1);
 
 namespace PoP\API\Cache;
 
+use PoP\Hooks\Facades\HooksAPIFacade;
 use PoP\ComponentModel\State\ApplicationState;
 
 class CacheUtils
 {
+    public const HOOK_SCHEMA_CACHE_KEY_COMPONENTS = __CLASS__ . ':schema-cache-key-components';
+
     public static function getSchemaCacheKeyComponents(): array
     {
         $vars = ApplicationState::getVars();
-        return [
-            'namespaced' => $vars['namespace-types-and-interfaces'],
-            'version-constraint' => $vars['version-constraint'],
-            'field-version-constraints' => $vars['field-version-constraints'],
-            'directive-version-constraints' => $vars['directive-version-constraints'],
-        ];
+        $hooksAPI = HooksAPIFacade::getInstance();
+        return (array)$hooksAPI->applyFilters(
+            self::HOOK_SCHEMA_CACHE_KEY_COMPONENTS,
+            [
+                'namespaced' => $vars['namespace-types-and-interfaces'],
+                'version-constraint' => $vars['version-constraint'],
+                'field-version-constraints' => $vars['field-version-constraints'],
+                'directive-version-constraints' => $vars['directive-version-constraints'],
+            ]
+        );
     }
 }
