ae6e4441246ffe8976bd9ec8e87da7c55fff9221
Leonardo Losoviz <leo@getpop.org>
2020-10-17 22:04:30 +0800

    Use `getenv` instead of `$_ENV` To support SetEnv in .htaccess

--- src/Environment.php
+++ src/Environment.php
@@ -12,6 +12,6 @@ class Environment
 
     public static function disableAPI(): bool
     {
-        return isset($_ENV['DISABLE_API']) ? strtolower($_ENV['DISABLE_API']) == "true" : false;
+        return getenv('DISABLE_API') !== false ? strtolower(getenv('DISABLE_API')) == "true" : false;
     }
 }
