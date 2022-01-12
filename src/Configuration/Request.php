42501ac37b27bb273a1c053a84f7f91a95c03e1e
leoloso <leo@getpop.org>
2022-01-12 11:12:12 +0000

    Merge pull request #1345 from leoloso/Remove-stale-EndpointExecuter-registries
    Remove stale `EndpointExecuter` registries

--- src/Configuration/Request.php
+++ src/Configuration/Request.php
@@ -5,21 +5,28 @@ declare(strict_types=1);
 namespace PoP\API\Configuration;
 
 use PoP\API\Constants\Params;
+use PoP\API\Environment;
 
 class Request
 {
     public static function mustNamespaceTypes(): ?bool
     {
-        if (isset($_REQUEST[Params::USE_NAMESPACE])) {
-            return in_array(
-                strtolower($_REQUEST[Params::USE_NAMESPACE]),
-                [
-                    "true",
-                    "on",
-                    "1"
-                ]
-            );
+        if (!Environment::enableSettingNamespacingByURLParam()) {
+            return null;
         }
-        return null;
+
+        $useNamespace = $_REQUEST[Params::USE_NAMESPACE] ?? null;
+        if ($useNamespace === null) {
+            return null;
+        }
+
+        return in_array(
+            strtolower($useNamespace),
+            [
+                "true",
+                "on",
+                "1"
+            ]
+        );
     }
 }
