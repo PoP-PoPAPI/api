16331184dfd5ef0d957aa8712ff6e985c558b420
leoloso <leo@getpop.org>
2022-08-24 00:41:31 +0000

    Merge pull request #1790 from leoloso/Move-moveEntriesWithoutIDUnderDBName-to-service
    Move `moveEntriesWithIDUnderDBName` from Engine to new service

--- src/Hooks/DBEntriesHookSet.php
+++ src/Hooks/DBEntriesHookSet.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PoPAPI\API\Hooks;
 
-use PoP\ComponentModel\Engine\Engine;
+use PoP\ComponentModel\Response\DatabaseEntryManager;
 use PoP\Root\App;
 use PoP\Root\Hooks\AbstractHookSet;
 
@@ -13,13 +13,17 @@ class DBEntriesHookSet extends AbstractHookSet
     protected function init(): void
     {
         App::addFilter(
-            Engine::HOOK_DBNAME_TO_FIELDNAMES,
+            DatabaseEntryManager::HOOK_DBNAME_TO_FIELDNAMES,
             $this->moveEntriesUnderDBName(...),
             10,
             1
         );
     }
 
+    /**
+     * @param array<string,string[]> $dbNameToFieldNames
+     * @return array<string,string[]>
+     */
     public function moveEntriesUnderDBName(array $dbNameToFieldNames): array
     {
         // Enable to add all fields starting with "__" (such as "__schema") as meta
